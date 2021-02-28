6e8819d509b460abc5a9088ae9dbd8cb770ea177
samuelslavka <slavka.samuel@gmail.com>
2021-02-28 10:47:49 +0000

    node api

--- app.js
+++ app.js
@@ -1,25 +1,80 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 
 const path = __dirname + '/views/';
 const port = 8080;
 
-router.use(function (req,res,next) {
-  console.log('/' + req.method);
-  next();
+app.get('/', function(req, res){
+	var key = req.query['api-key'];
+	res.send('hello');
 });
 
-router.get('/',function(req,res){
-  res.sendFile(path + 'index.html');
+
+app.get('/:name', async function(req, res, next){
+	var name = req.params.name;
+	var cont = await searchitem(name);
+	res.send(cont);
+});
+  
+app.get('/:name/heading', async function(req, res, next){
+	var name = req.params.name;
+	res.send( await getHeading(name));
 });
 
-router.get('/sharks',function(req,res){
-  res.sendFile(path + 'sharks.html');
-});
+//execute search in wikipedia
+async function searchitem(str)
+{
+	try {
+		const { data } = await axios.get(
+			'https://en.wikipedia.org/w/index.php?search='+str+'&title=Special%3ASearch&fulltext=Search&ns0=1'
+		);
+		const $ = cheerio.load(data);
+		var result = [];
+		$('#mw-content-text')
+		//$('p.mw-empty-elt').remove();
+		if ($('p.mw-search-nonefound') != ''){
+			result.push($('p.mw-search-nonefound').text());    
+		}
+		else result = await getContents(str);
 
-app.use(express.static(path));
-app.use('/', router);
+		return result;
+	} catch (error) {
+		throw error;
+	}
+};
+
+//return contents of article, either first paragraph or itemize
+async function getContents(str) 
+{
+	try {
+		const { data } = await axios.get(
+			'https://en.wikipedia.org/wiki/'+str
+		);
+		const $ = cheerio.load(data);
+    
+		$('div.mw-parser-output:empty').remove();
+		$('p.mw-empty-elt').remove();
+		var content = []
+		content.push($('div.mw-parser-output > p:first').text());
+		
+
+		if (content[0].includes('may refer to:')){
+			$('div.mw-parser-output > ul').each((_idx, el) => {
+				const item = $(el).text();
+				content.push(item)
+			});
+		}
+		
+		return content;
+	} catch (error) {
+		throw error;
+	}
+};
+
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!')
--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/rpi/node_project/
+cd /home/rpi/slavkaone/
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
