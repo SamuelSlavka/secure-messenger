8d429a0d447159661cde33a0a50d0ccdfb2439d3
samuelslavka <slavka.samuel@gmail.com>
2021-06-02 17:15:31 +0200

    initial api

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+# compose_flask/app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Wellcome to flask'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
+
--- backend/router.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-
-// Registered entity routes
-router.use(require('./services/router'));
-
-module.exports = router;
