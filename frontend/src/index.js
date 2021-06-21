9cfbef3bafbca28e49a800f6f511a2158b8cd19b
SamuelSlavka <slavka.samuel@gmail.com>
2021-06-21 15:27:23 +0000

    cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import './App.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-
-serviceWorkerRegistration.register('service-worker.js');
 
 // main body
 ReactDOM.render(<App />, document.getElementById('root'))
-
-
-//reportWebVitals();
